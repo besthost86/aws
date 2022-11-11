3d1901cf563468f1a32f192233cf3c99e5a1a714
Besthost <besthostgambler@gmail.com>
2022-11-11 22:37:08 +0100

    change router order

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const app = express();
 
-app.use("/", (req, res) => res.json({message: "you did it"}));
 app.use("/api", (req, res) => res.json({message: "this is your api"}));
+app.use("/", (req, res) => res.json({message: "you did it"}));
 
 app.listen(8080, ()=> console.log("listening..."));
