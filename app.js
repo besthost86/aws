d3117697f8e1bdb0e61af464eb3f5902d00ea628
Besthost <besthostgambler@gmail.com>
2022-11-11 22:09:46 +0100

    change port

--- app.js
+++ app.js
@@ -5,4 +5,4 @@ const app = express();
 app.use("/", (req, res) => res.json({message: "you did it"}));
 app.use("/api", (req, res) => res.json({message: "this is your api"}));
 
-app.listen(process.env.PORT || 5001, ()=> console.log("listening..."));
+app.listen(5001, ()=> console.log("listening..."));
