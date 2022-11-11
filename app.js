6a85c765130ac27813b66977040182ecc6474486
Besthost <besthostgambler@gmail.com>
2022-11-11 21:37:11 +0100

    add new endpoint

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ import express from "express";
 const app = express();
 
 app.use("/", (req, res) => res.json({message: "you did it"}));
+app.use("/api", (req, res) => res.json({message: "this is your api"}));
 
 app.listen(process.env.PORT || 5001, ()=> console.log("listening..."));
